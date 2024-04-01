b2f4aeb303e19c36d4e8dd0118766cdf87e2dc67
@rikatussei <yohei.arisu@Gmail.com>
2024-04-01 11:29:37 +0900

    database.yml記述修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'items/index'
+  get 'items/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'items#index'
+  resources :items
 
   # Defines the root path route ("/")
   # root "articles#index"
