65f38a7cdf91bf3f4f3e9e1fb2a7f9a6a29870e4
@rikatussei <yohei.arisu@Gmail.com>
2024-04-02 19:33:00 +0900

    バリデーションの修正前

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,16 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
 
+  validate  :password_complexity
+
+  private
+
+  def password_complexity
+    return if password.blank? || password =~ /^(?=.*[a-zA-Z])(?=.*[0-9]).{6,}$/
+
+    errors.add :password, 'is invalid. Include both letters and numbers'
+  end
+
   # 後で有効化
   # has_many :items, dependent: :destroy
   # has_many :orders, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
   resources :items
+  resources :users
 
   # Defines the root path route ("/")
   # root "articles#index"
