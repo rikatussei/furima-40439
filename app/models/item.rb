7b319c1170644894efc4a973bd7c55526e366a00
@rikatussei <yohei.arisu@Gmail.com>
2024-04-04 19:33:06 +0900

    2024年インデックスページ差し戻し後

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,18 +8,27 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true
   validates :delivery_time_id, presence: true
   validates :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, format: { with: /\A\d+\z/, message: "は半角数字のみ有効です" }
   validates :image, presence: true
 
   has_one_attached :image
 
-
-  # validates :content, presence: true, unless: :was_attached?
-
-  # def was_attached?
-    # self.image.attached?
-  # end
-
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :delivery_time
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_time_id, numericality: { other_than: 1 , message: "can't be blank"}
+
   # 後で有効化
   # has_one    :order, dependent: :destroy
 end
