8b5f799e4f5d8604f180d15dad57d18a40da7cff
@rikatussei <yohei.arisu@Gmail.com>
2024-04-07 17:11:33 +0900

    商品の一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-
+  # before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
 
   def index
-    # @items = Item.includes(:user)
+    @items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -19,8 +19,8 @@ class ItemsController < ApplicationController
     end
   end
   
-  def show
-  end
+  # def show
+  # end
 
 private
 
