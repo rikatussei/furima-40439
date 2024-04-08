8cc134a443849211978bb89ebf25c2b95e1ea0ce
@rikatussei <yohei.arisu@Gmail.com>
2024-04-08 14:11:00 +0900

    ビフォアアクションを定義しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to @item, notice: 'Item was successfully updated.'
     else
@@ -43,10 +41,16 @@ class ItemsController < ApplicationController
                                  :prefecture_id, :delivery_time_id, :price, :image).merge(user_id: current_user.id)
   end
 
+
   def move_to_index
     @item = Item.find(params[:id])
-    return unless current_user.id != @item.user_id
-
+    if current_user.id != @item.user_id
     redirect_to action: :index
+    end
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
