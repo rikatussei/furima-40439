c2ee51967afb59c41af6f04da0a7962db1e4421e
@rikatussei <yohei.arisu@Gmail.com>
2024-04-08 11:52:32 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -23,9 +24,16 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to @item, notice: 'Item was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
@@ -33,5 +41,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:product_name, :product_description, :category_id, :item_condition_id, :shipping_cost_id,
                                  :prefecture_id, :delivery_time_id, :price, :image).merge(user_id: current_user.id)
+  end
+end
+
+def move_to_index
+  @item = Item.find(params[:id])
+  if current_user.id != @item.user_id
+    redirect_to action: :index
   end
 end
