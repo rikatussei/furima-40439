3cc2f351913ae708c6cd2ea7fce6570bf1c2ef84
@rikatussei <yohei.arisu@Gmail.com>
2024-04-09 15:49:51 +0900

    マイグレーションファイル編集

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,13 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
   resources :items
+  resources :orders, only: [:index, :new, :create]
   resources :users
 
   # Defines the root path route ("/")
